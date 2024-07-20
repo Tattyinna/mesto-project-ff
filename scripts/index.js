cae949c3e1c79bd2f1ef512e2d49cd09e490d672
Tatiana <iuschina.t@yandex.ru>
2024-07-20 21:27:47 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+ 
 // @todo: DOM узлы
+const cardTemplate = document.querySelector("#card-template").content;
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, handleDelete) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+
+  cardElement.querySelector(".card__delete-button").addEventListener("click", handleDelete);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function handleDelete(event) {
+  event.target.closest(".card").remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, handleDelete);
+  cardContainer.append(cardElement);
+});
